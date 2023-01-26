99591d21903d4c17ae15d88986e8fc5642787b1e
gideon <gideon.monterosa@gmail.com>
2023-01-26 22:23:38 +0100

    make the animation more smooth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,75 @@
 <script lang="ts">
-    import Piece from "./Piece.svelte";
-    import {STANDARD_BOARD} from "./Boards";
+	import Piece from './Piece.svelte';
+	import { STANDARD_BOARD } from './Boards';
+	import { onMount } from 'svelte';
+	import { Board } from './Board';
 
-    let board = STANDARD_BOARD
+	let board = STANDARD_BOARD;
+	let selected: any = null;
+	let selectedCoordinates: number[];
 
-    const dragStart = (e, rowIndex, fieldIndex) => {
-        console.log('dragstart')
-        e.dataTransfer.setData('rowIndex', rowIndex)
-        e.dataTransfer.setData('fieldIndex', fieldIndex)
-    }
+	const getTileCoordinates = (e): number[] => {
+		return document
+			.elementsFromPoint(e.clientX, e.clientY)
+			.find((el) => {
+				return el.classList.contains('tile');
+			})
+			.getAttribute('coordinates')
+			.split(',')
+			.map((el) => parseInt(el));
+	};
 
-    const drop = (e, rowIndex, fieldIndex) => {
-        console.log('drop')
-        const fromRowIndex = e.dataTransfer.getData('rowIndex')
-        const fromFieldIndex = e.dataTransfer.getData('fieldIndex')
-        const fromField = board[fromRowIndex][fromFieldIndex]
+	const dragStart = (e) => {
+		selectedCoordinates = getTileCoordinates(e);
+		selected = e.target;
+		drag(e);
+	};
 
-        board[fromRowIndex][fromFieldIndex] = ''
-        board[rowIndex][fieldIndex] = fromField
-    }
+	const drag = (e) => {
+		if (selected) {
+			e.preventDefault();
+			selected.style.position = 'absolute';
+			selected.style.top = e.clientY - 22 + 'px';
+			selected.style.left = e.clientX - 22 + 'px';
+		}
+	};
+
+	const drop = (e) => {
+		if (selected) {
+			selected.style.position = 'static';
+			selected.style.top = '0';
+			selected.style.left = '0';
+
+			selected = null;
+
+			const coordinates = getTileCoordinates(e);
+			const fromField = board[selectedCoordinates[0]][selectedCoordinates[1]];
+
+			board[selectedCoordinates[0]][selectedCoordinates[1]] = '';
+			board[coordinates[0]][coordinates[1]] = fromField;
+		}
+	};
+
+	onMount(() => {
+		document.addEventListener('mousemove', drag);
+	});
 </script>
 
 <div class="w-screen h-screen flex justify-center items-center">
-    <div class="w-96 h-96 flex justify-center content-center flex-wrap">
-        {#each board as row, i}
-            {#each row as field, j}
-                <div class="h-12 w-12 {((i % 2) + j) % 2 ? 'bg-red-400' : 'bg-white'}"
-                     on:drop={e => drop(e, i, j)}
-                     ondragover="return false">
-                    {#if field !== ''}
-                        <div draggable="true" on:dragstart={e => dragStart(e, i, j)}>
-                            <Piece pieceType="{field}"/>
-                        </div>
-                    {/if}
-                </div>
-            {/each}
-        {/each}
-    </div>
+	<div class="w-96 h-96 flex justify-center content-center flex-wrap">
+		{#each board as row, i}
+			{#each row as field, j}
+				<div
+					class="tile h-12 w-12 {((i % 2) + j) % 2 ? 'bg-red-400' : 'bg-white'}"
+					coordinates="{i},{j}"
+				>
+					{#if field !== ''}
+						<div draggable="true" on:mousedown={(e) => dragStart(e)} on:mouseup={drop}>
+							<Piece pieceType={field} />
+						</div>
+					{/if}
+				</div>
+			{/each}
+		{/each}
+	</div>
 </div>
